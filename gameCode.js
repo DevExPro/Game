6cbf919f17783d932a9a5f0eca365ce922f05847
greengreen12 <lilymcox@gmail.com>
2016-08-30 23:56:38 +0000

    New restart button

--- gameCode.js
+++ gameCode.js
@@ -4,13 +4,13 @@ var gameHeight = window.innerHeight - 100;
 var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
 function preload () {
-  game.load.image('background', '../images/back.png');
+  game.load.image('background', '/images/back.png');
   game.load.image('bullet', '../images/pewpew2.png');
   game.load.image('fire', '../images/emit.png');
   game.load.image('ship2', '../images/shipGreen.png');
   game.load.spritesheet('boom1', '../images/expolode2.png', 39, 38, 7);
-  game.load.image('enemy', '../images/enemyShip2.png');
-  game.load.image('heart', '../images/lifeShip.png');
+  game.load.image('enemy', 'images/enemyShip2.png');
+  game.load.image('heart', 'images/lifeShip.png');
   game.load.image('powerUp', '../images/lightning2.png');
   game.load.spritesheet('beam', '../images/laserBeam.png', 40, 16, 11);
   game.load.spritesheet('pauseButton', '../images/pause.png', 36, 36, 2);
@@ -19,7 +19,7 @@ function preload () {
   game.load.image('title', '../images/evasion2.png');
   game.load.image('gameOver', '../images/gameOverTitle.png');
   game.load.image('winTitle', '../images/victory.png');
-  game.load.image('restartButton', '../images/restartButton.png');
+  game.load.image('restartButton', '../images/restartButton2.png');
   game.load.image('playerShield', '../images/shield.png');
   game.load.image('powerEnemy', '../images/Ship4.png');
   game.load.image('shieldMove', '../images/shieldMove.png');
